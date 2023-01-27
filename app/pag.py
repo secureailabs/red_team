06855f7c256aa3208fe0e993143dc197c4b88724
Stanley <stanley@secureailabs.com>
2023-01-27 13:42:16 -0500

    update Pag template info

--- app/pag.py
+++ app/pag.py
@@ -26,5 +26,18 @@ pag_bp = Blueprint("pag_bp", __name__, template_folder="templates")
 def pag_info():
     pag_id = json.loads(request.args["messages"]).get("username")
     pag = db.user.find_one({"username": pag_id})
+    patient_records = db.user.find({"role": "patient"})
+    list_patient = []
+    for patient in patient_records:
+        list_patient.append(patient)
 
-    return render_template("pag.html", pag=pag, team_name="Team Red")
+    return render_template(
+        "pag.html", pag=pag, patients=list_patient, patient_id=None, patient_records=None, team_name="Team Red"
+    )
+
+
+def patient_records(user_id):
+    patient_info = db.record.find_one({"id": user_id})
+    patient_records = patient_info.get("records")
+
+    return patient_records
