786c2e15d6ab28d8805473ce0583588c91c646a2
Stanley <stanley@secureailabs.com>
2023-01-28 05:45:05 -0500

    PAG added patient record information

--- app/pag.py
+++ app/pag.py
@@ -15,29 +15,99 @@
 
 import json
 
+from bson.objectid import ObjectId
 from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask.views import MethodView
 
 from server import db
 
 pag_bp = Blueprint("pag_bp", __name__, template_folder="templates")
 
 
-@pag_bp.route("/pag", methods=("GET", "POST"))
-def pag_info():
-    pag_id = json.loads(request.args["messages"]).get("username")
-    pag = db.user.find_one({"username": pag_id})
-    patient_records = db.user.find({"role": "patient"})
+def get_list_patients():
+    patient_info = db.user.find({"role": "patient"})
     list_patient = []
-    for patient in patient_records:
+    for patient in patient_info:
         list_patient.append(patient)
-
-    return render_template(
-        "pag.html", pag=pag, patients=list_patient, patient_id=None, patient_records=None, team_name="Team Red"
-    )
+    return list_patient
 
 
-def patient_records(user_id):
-    patient_info = db.record.find_one({"id": user_id})
-    patient_records = patient_info.get("records")
+def get_patient_info(patient_id):
+    patient_info = db.user.find_one({"_id": ObjectId(patient_id)})
+    return patient_info
 
-    return patient_records
+
+class PagView(MethodView):
+    init_every_request = False
+
+    def __init__(
+        self,
+    ):
+        self.pag_info = {}
+
+    def get(
+        self,
+    ):
+        self.pag_info = json.loads(request.args["messages"])
+        return render_template(
+            "pag.html",
+            pag=self.pag_info,
+            patients=get_list_patients(),
+            patient_info=None,
+            team_name="Team Red",
+        )
+
+    def post(
+        self,
+    ):
+        if "check_record" in request.form:
+            patient_id = request.form["check_record"]
+            patient_info = get_patient_info(patient_id)
+            return render_template(
+                "pag.html",
+                pag=self.pag_info,
+                patients=get_list_patients(),
+                patient_info=patient_info,
+                team_name="Team Red",
+            )
+
+        return render_template(
+            "pag.html",
+            pag=self.pag_info,
+            patients=get_list_patients(),
+            patient_info=None,
+            team_name="Team Red",
+        )
+
+
+pag_view = PagView.as_view("pag_info")
+pag_bp.add_url_rule("/pag", view_func=pag_view)
+
+
+# @pag_bp.route("/pag", methods=("GET", "POST"))
+# def pag_info():
+#     pag_id = json.loads(request.args["messages"]).get("username")
+#     pag = db.user.find_one({"username": pag_id})
+#     patient_records = db.user.find({"role": "patient"})
+#     list_patient = []
+#     for patient in patient_records:
+#         list_patient.append(patient)
+
+#     return render_template(
+#         "pag.html", pag=pag, patients=list_patient, patient_id=None, patient_records=None, team_name="Team Red"
+#     )
+
+
+@pag_bp.route("/pag/<id>/")
+def default(id):
+    # pag_id = json.loads(request.args["messages"]).get("username")
+    # pag = db.user.find_one({"username": pag_id})
+    # patient_records = db.user.find({"_id": id})
+    return "the value is:" + id
+    # return render_template(
+    #     "pag.html",
+    #     pag=pag,
+    #     patients=get_patients_info(),
+    #     patient_info=patient_records,
+    #     team_name="Team Red",
+    # )
