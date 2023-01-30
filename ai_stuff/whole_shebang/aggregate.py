35b2fedee452968005c0918c51e92a8d863b415b
AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
2023-01-30 17:38:36 +0700

    aggregator in

--- ai_stuff/whole_shebang/aggregate.py
+++ ai_stuff/whole_shebang/aggregate.py
@@ -5,10 +5,19 @@ def read_csv(file_name):
     txt_file.close()
     return content_list
 
-questions = read_csv("patient_1/questions.txt") + read_csv("patient_2/questions.txt")
+def aggregate_files(file_name):
+    list = read_csv("patient_1/"+file_name+".txt") + read_csv("patient_2/"+file_name+".txt")
+    rep = []
+    for element in list:
+        rep.append(element.replace("\n", ""))
+    list = rep
+    with open("agg_"+file_name+".txt", 'w') as f:
+        print(*list, sep=";", file=f)
 
-rep = []
-for question in questions:
-    rep.append(question.replace("\n", ""))
-questions = rep
-print(questions)
+aggregate_files("questions")
+aggregate_files("raw_keywords")
+aggregate_files("symptoms")
+aggregate_files("verbs")
+aggregate_files("names")
+aggregate_files("orgs")
+aggregate_files("dates")
